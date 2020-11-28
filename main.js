16efa9536dc90a006f7142951a5490f92409027c
Víctor <systemdeveloper868@gmail.com>
2020-11-29 00:12:15 +0100

    basico

--- main.js
+++ main.js
@@ -4,24 +4,22 @@ const puppeteer = require('puppeteer');
     const browser = await puppeteer.launch({
         headless: false
     });
+    /* Dirección */
     const page = await browser.newPage();
     await page.goto("https://reactjs.org/");
-    /*const title = await page.url();
-    console.log(title, url);
-    await page.type('#twotabsearchtextbox', 'naranjas');
-    await page.click('.nav-input input');
-    // #search
-    */
-   const leerDatos = await page.evaluate(()=>{
-    const datosCapturados = document.querySelector('.css-1s44ra')
-    return datosCapturados.innerHTML 
-   })
-   
+    /* Acciones a realizar */
+    const leerDatos = await page.evaluate(() => {
+        const datosCapturados = document.querySelectorAll('.css-1s44ra')
+        return datosCapturados[0].innerHTML
+    })
+    console.log(leerDatos)
+    
+    /* captura imagen de pantalla */
     await page.screenshot({
         path: 'example.png'
     });
 
-    console.log(leerDatos)
-
+    
+    /* siempre cerrar */
     await browser.close();
 })();
